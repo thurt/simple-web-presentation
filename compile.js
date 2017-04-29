d0cb0e06c2bda72e191080851fd8b84945268377
thurt <thurt@madmobile.com>
2017-04-29 15:28:44 -0400

    removes extra /presentation directory when creating a presentation -- this means that files will be copied directly into the outputDir provided by the user; adds npm script demo -- this creates the demo project in the /demo dir; renames npm script compile -> compileJs;

--- compile.js
+++ compile.js
@@ -19,20 +19,17 @@ const page_template = handlebars.compile(page)
 const icon_template = handlebars.compile(icon)
 
 // create the directory to place the files into
-if (!fs.existsSync(path.join(outputDir, '/presentation'))) {
-  fs.mkdirSync(path.join(outputDir, '/presentation'))
-}
-if (!fs.existsSync(path.join(outputDir, '/presentation/css'))) {
-  fs.mkdirSync(path.join(outputDir, '/presentation/css'))
+if (!fs.existsSync(path.join(outputDir, '/css'))) {
+  fs.mkdirSync(path.join(outputDir, '/css'))
 }
 
 // create the html file
-fs.writeFileSync(path.join(process.cwd(), outputDir, '/presentation/index.html'), page_template(presentation_data))
+fs.writeFileSync(path.join(process.cwd(), outputDir, '/index.html'), page_template(presentation_data))
 
 // create the icon file
 fs.writeFileSync(path.join(__dirname, '/compile/favicon.svg'), icon_template(presentation_data))
 
-icongen(path.join(__dirname, '/compile/favicon.svg'), path.join(process.cwd(), outputDir, '/presentation'), {
+icongen(path.join(__dirname, '/compile/favicon.svg'), path.join(process.cwd(), outputDir), {
   modes: ['ico'],
   names: {
     ico: 'favicon'
@@ -41,12 +38,12 @@ icongen(path.join(__dirname, '/compile/favicon.svg'), path.join(process.cwd(), o
 
 // copy over the bundle.js
 const bundle = fs.readFileSync(path.join(__dirname, '/bundle.js'))
-fs.writeFileSync(path.join(process.cwd(), outputDir, '/presentation/bundle.js'), bundle)
+fs.writeFileSync(path.join(process.cwd(), outputDir, '/bundle.js'), bundle)
 
 // copy over css files
 const baseCss = fs.readFileSync(path.join(__dirname, '/css/base.css'))
 const customCss = fs.readFileSync(path.join(__dirname, '/css/custom.css'))
 const themeCss = fs.readFileSync(path.join(__dirname, '/css/default.theme.css'))
-fs.writeFileSync(path.join(process.cwd(), outputDir, '/presentation/css/base.css'), baseCss)
-fs.writeFileSync(path.join(process.cwd(), outputDir, '/presentation/css/custom.css'), customCss)
-fs.writeFileSync(path.join(process.cwd(), outputDir, '/presentation/css/default.theme.css'), themeCss)
+fs.writeFileSync(path.join(process.cwd(), outputDir, '/css/base.css'), baseCss)
+fs.writeFileSync(path.join(process.cwd(), outputDir, '/css/custom.css'), customCss)
+fs.writeFileSync(path.join(process.cwd(), outputDir, '/css/default.theme.css'), themeCss)
